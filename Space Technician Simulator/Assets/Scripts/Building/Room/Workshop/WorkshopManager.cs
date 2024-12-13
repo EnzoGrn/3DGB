ed61378e07256e39a3495e361f8ea29b0b59db80
PharaEthan <ethan1.hernou@epitech.eu>
2024-12-14 01:02:11 +0900

    feat(Workshop): New room with electricity off

---
+++ Space Technician Simulator/Assets/Scripts/Building/Room/Workshop/WorkshopManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkshopManager : ARoom {
+   
+    [Header("Hangar Elements")]
+
+    [SerializeField]
+    [Tooltip("The lights of the hangar")]
+    private GameObject[] _Lights;
+
+    [SerializeField]
+    [Tooltip("The door of the hangar")]
+    private AreaTriggerDoor[] _AreaDoor;
+
+    public override void SetLight(bool isLightOn)
+    {
+        // -- Put the lights on or off, when interuptor change state
+        for (int i = 0; i < _Lights.Length; i++)
+            _Lights[i].SetActive(IsLightOn);
+
+        // -- Block or unblock the door, when interuptor change state
+        for (int i = 0; i < _AreaDoor.Length; i++) {
+            _AreaDoor[i].LockDoor(false);
+
+            if (IsLightOn)
+                _AreaDoor[i].Close(false);
+            else
+                _AreaDoor[i].Open(false);
+            _AreaDoor[i].LockDoor(!IsLightOn);
+        }
+    }
+
+    public void Awake()
+    {
+        OnElectricityChange += SetLight;
+    }
+}
